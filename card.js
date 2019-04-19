9c23e8da04d06d7e444fe669ad5923b66f802c0d
Smittyvb <smittyvb@gmail.com>
2019-04-19 15:18:58 -0400

    Create card.js

---
+++ card.js
@@ -0,0 +1,24 @@
+class Card {
+  constructor(id) {
+    this.id = id;
+    const dataReq = fetch("https://api.steemmonsters.io/cards/find?ids=" + id);
+    this.dataReq = dataReq;
+    (async function () {
+      //this gets run in the background
+      await dataReq;
+      const json = await dataReq.json();
+      if (json[0].error) return console.error("Couldn't fetch card data for", id);
+      this.cardData = json[0];
+    })();
+  }
+  async toHTML() {
+    await dataReq;
+    return (this.cardData.gold ? "gold " : "") + this.cardData.details.name;
+  }
+  async toExpandedHTML() {
+    await dataReq;
+  }
+  async toPageHTML() {
+    await dataReq;
+  }
+}
