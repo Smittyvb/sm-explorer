6c929cbc5e64b0e0970c2496a10558183cc8b3d0
Smittyvb <smittyvb@gmail.com>
2019-04-20 07:31:17 -0400

    Update card.js

--- card.js
+++ card.js
@@ -13,6 +13,32 @@ class Card {
       resolve();
     })();
   }
+  static getImageUrl(card) {
+		var details = card.details;
+		
+		if (!details)
+			return null;
+    
+    let betaCardUrl = "https://s3.amazonaws.com/steemmonsters/cards_beta/";
+    let otherCardUrl = "https://s3.amazonaws.com/steemmonsters/cards_v2.2/";
+
+		return ((card.edition == 1 || card.edition == 3) ? betaCardUrl : Config.otherCardUrl) + details.name + (card.gold ? '_gold' : '') + '.png';
+	}
+  async cardHTML() {
+    return `
+<div style="cursor: default;" id="card_${this.cardData.id}">
+  <img src="<%= Card.getImageUrl({ card_detail_id: data.id, gold: data.gold, edition: data.edition }) %>" class="card-img" card_detail_id="<%= data.id %>"/>
+  <% var level_info = SM.GetCardLevelInfo(data); %>
+  <div class="relative-position">
+    <div class="card-level <%= data.gold ? 'gold' : '' %>">
+      <div class="card-level-progress" style="width: <%= ((level_info.xp_needed > 0) ? level_info.xp_to_next_level / level_info.xp_needed * 100 : 100).toFixed(0) %>%;"></div>
+    </div>
+  </div>
+  <%= SM.ShowComponent('stats', data) %>		
+  <%= SM.ShowComponent('card_name', data) %>
+</div>
+    `;
+  }
   async toHTML() {
     await this.ready;
     return (this.cardData.gold ? "gold " : "") + this.cardData.details.name;
